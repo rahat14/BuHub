da129944b9a68c5e53c9dce72359835cee40dd3f
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2019-01-03 02:29:56 +0600

    v.1.5.5
    adding blood section

---
+++ app/src/main/java/com/metacodersbd/myapplication/bloodSection.java
@@ -0,0 +1,13 @@
+package com.metacodersbd.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bloodSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_section);
+    }
+}
